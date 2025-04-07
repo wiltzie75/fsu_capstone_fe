9d9d2c72806e28033c8f35df75e77a1caa3536e5
Dan Sealy <dan.sealy5@gmail.com>
2025-04-07 11:46:34 -0400

    react-router

--- fsu-capstone/src/App.jsx
+++ fsu-capstone/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,18 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route />
+      </Routes>
     </>
   )
 }
