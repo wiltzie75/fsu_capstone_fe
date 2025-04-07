df9869258359a91ca5ce274753e6dbea599aa1eb
Dan Sealy <dan.sealy5@gmail.com>
2025-04-07 13:51:06 -0400

    removing vite/react logos

--- fsu-capstone/src/App.jsx
+++ fsu-capstone/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route, Link } from 'react-router-dom'
 import Navbar from './components/Navbar'
