ccfe2f1c2d4645a8e38126badf34a525e401874c
Ben Hogle <ben.hogle32@gmail.com>
2025-04-08 13:40:36 -0400

    commit

--- fsu-capstone/src/App.jsx
+++ fsu-capstone/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Departments from "./components/Departments";
+import Faculty from "./components/Faculty";
+import DepartmentDetailPage from "./components/DepartmentDetailPage";
+import FacultyDetailPage from "./components/FacultyDetailPage";
+import AdminPage from "./components/AdminPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,11 +23,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <Routes>
-        <Route />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/faculty" element={<Faculty />} />
+          <Route path="/department/:id" element={<DepartmentDetailPage />} />
+          <Route path="/faculty/:id" element={<FacultyDetailPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ fsu-capstone/src/components/Register.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { SlLock } from "react-icons/sl";
+
+const Register = ({ setToken }) => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("", {
+        method: "POST",
+        body: JSON.stringify({ firstname, lastname, email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      setToken(result.token);
+
+      setFirstname("");
+      setLastname("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console(error);
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="signup-header">
+        <div className="text">Register</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={<SlLock />} alt="Password Icon" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className="submit-container">
+        <div className="submit">Register</div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
