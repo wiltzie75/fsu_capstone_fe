6fb06f9aa301ee59d2326356be474012a43a91b8
Dan Sealy <dan.sealy5@gmail.com>
2025-04-08 13:06:35 -0400

    lincoln image

--- fsu-capstone/src/components/Navbar.jsx
+++ fsu-capstone/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Link, Navigate, NavLink } from 'react-router-dom';
+import Lincoln from "/assets/Lincoln.png"
 
 const Navbar = ({ isLoggedIn, handleLoginLogout }) => {
 
     return (
         <div className="navbar" role="banner">
         <NavLink to ="/" className="logo">
-            <img src="https://media.istockphoto.com/id/1184567639/vector/ecology-sphere-logo-formed-by-twisted-green-leaves.jpg?s=612x612&w=0&k=20&c=RuhMuEHwiUk50BonfU2yDBpo3IOBa7nywRdY069-TCU=" className="logo" alt="LU logo" />
+            <img src="/assets/Lincoln.png" className="logo" alt="LU logo" />
         </NavLink>
         <div className="navbar-title">
         <h1>Lincoln University</h1>
