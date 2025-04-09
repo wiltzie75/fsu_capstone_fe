26907d305ea69094bf901215756092c57cb1bb9a
Dan Sealy <dan.sealy5@gmail.com>
2025-04-09 11:23:11 -0400

    edit

--- fsu-capstone/src/App.jsx
+++ fsu-capstone/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import "./App.css
+import "./app.css"
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Departments from "./components/Departments";
-import Faculty from "./components/Faculty";
-import DepartmentDetailPage from "./components/DepartmentDetailPage";
-import FacultyDetailPage from "./components/FacultyDetailPage";
-import AdminPage from "./components/AdminPage";
+// import Home from "./components/Home";
+// import Departments from "./components/Departments";
+// import Faculty from "./components/Faculty";
+// import DepartmentDetailPage from "./components/DepartmentDetailPage";
+// import FacultyDetailPage from "./components/FacultyDetailPage";
+// import AdminPage from "./components/AdminPage";
 import Navbar from "./components/Navbar";
 import Register from "./components/register";
 
@@ -21,21 +21,6 @@ function App() {
 
   return (
     <>
-
-      <Navbar isLoggedIn={isLoggedIn} />
-      <div></div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/departments" element={<Departments />} />
-          <Route path="/faculty" element={<Faculty />} />
-          <Route path="/department/:id" element={<DepartmentDetailPage />} />
-          <Route path="/faculty/:id" element={<FacultyDetailPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </Router>
-      );
-
     <Navbar isLoggedIn={isLoggedIn}/>
       <Routes>
         <Route path="/" />
@@ -47,7 +32,6 @@ function App() {
         {/* <Route path="/faculty/:id" element={<FacultyDetailPage />} /> */}
         {/* <Route path="/admin" element={<AdminPage />} /> */}
       </Routes>
-
     </>
   );
 }
